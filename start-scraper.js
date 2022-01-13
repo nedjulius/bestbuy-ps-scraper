8ddbdc30b5f9631073bbd6a5ac7230afb5120899
nedjulius <julius.nedzinskas@gmail.com>
2022-01-13 17:52:00 +0200

    Change puppeteer script

--- start-scraper.js
+++ start-scraper.js
@@ -4,33 +4,53 @@ const {
 } = require('./mailer')
 const {BESTBUY_PS_DIGITAL_URL} = require('./constants')
 const cron = require('node-cron')
-const axios = require('axios')
-const cheerio = require('cheerio')
+const puppeteer = require('puppeteer')
 
 const scrapeForBuyButtonText = async () => {
-  const {data} = await axios.get(BESTBUY_PS_DIGITAL_URL)
-  const $ = cheerio.load(data)
+  const browser = await puppeteer.launch({
+    headless: true,
+    ignoreHTTPSErrors: true,
+    args: [`--window-size=1920,1080`, '--no-sandbox'],
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
+  })
 
-  return $('.fulfillment-add-to-cart-button').text()
+  const page = await browser.newPage()
+  await page.setUserAgent(
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+  )
+  await page.setCacheEnabled(false)
+
+  await page.goto(BESTBUY_PS_DIGITAL_URL)
+  await page.waitForSelector('.fulfillment-add-to-cart-button', {visible: true})
+
+  const soldOutBtn = await page.$('.fulfillment-add-to-cart-button')
+  const soldOutBtnText = await page.evaluate((el) => el.textContent, soldOutBtn)
+
+  await browser.close()
+
+  return soldOutBtnText.trim()
 }
 
-(async () => {
-	const text = await scrapeForBuyButtonText();
-	console.log(text)
+;(async () => {
+  const text = await scrapeForBuyButtonText()
+  console.log(text)
 })()
 
-// const cronJob = cron.schedule('*/1 * * * *', async () => {
-//   try {
-//     const buyButtonText = await scrapeForBuyButtonText()
+const cronJob = cron.schedule('*/1 * * * *', async () => {
+  try {
+    const buyButtonText = await scrapeForBuyButtonText()
 
-//     if (buyButtonText !== 'Sold Out') {
-//       await sendFoundNotificationMail()
-//       cronJob.stop()
-//     } else {
-//       console.log('Not found... Keep on working!')
-//     }
-//   } catch (error) {
-//     await sendErrorNotificationMail(error)
-//     cronJob.stop()
-//   }
-// })
+    if (buyButtonText !== 'Sold Out') {
+      await sendFoundNotificationMail()
+      cronJob.stop()
+    } else {
+      console.log('Not found... Keep on working!')
+    }
+  } catch (error) {
+    await sendErrorNotificationMail(error)
+    cronJob.stop()
+  }
+})
