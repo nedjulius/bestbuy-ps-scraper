a8e71e1440aacfb7c3534e91d4a7b0433f006d25
nedjulius <julius.nedzinskas@gmail.com>
2022-01-13 16:08:34 +0200

    Add axios and cheerio

--- start-scraper.js
+++ start-scraper.js
@@ -3,38 +3,14 @@ const {
   sendErrorNotificationMail,
 } = require('./mailer')
 const {BESTBUY_PS_DIGITAL_URL} = require('./constants')
-const cron = require('node-cron')
-const puppeteer = require('puppeteer')
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const scrapeForBuyButtonText = async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    ignoreHTTPSErrors: true,
-    args: [`--window-size=1920,1080`],
-    defaultViewport: {
-      width: 1920,
-      height: 1080,
-    },
-  })
+	const {data} = await axios.get(BESTBUY_PS_DIGITAL_URL);
+	const $ = cheerio.load(data);
 
-  const page = await browser.newPage()
-  await page.setUserAgent(
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
-  )
-  await page.setCacheEnabled(false)
-
-  await page.goto(BESTBUY_PS_DIGITAL_URL)
-  await Promise.all([
-    page.waitForNavigation({waitUntil: 'networkidle2'}),
-    page.click('.us-link'),
-  ])
-
-  const soldOutBtn = await page.$('.fulfillment-add-to-cart-button')
-  const soldOutBtnText = await page.evaluate((el) => el.textContent, soldOutBtn)
-
-  await browser.close()
-
-  return soldOutBtnText.trim()
+	return $('.fulfillment-add-to-cart-button').text();
 }
 
 const cronJob = cron.schedule('*/30 * * * *', async () => {
